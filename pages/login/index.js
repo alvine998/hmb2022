f29ddc39b27f3590491ba3c305c5b2979c1a0707
alvine <alvinecom2018@gmail.com>
2022-02-02 23:26:05 +0700

    fixing login

--- pages/login/index.js
+++ pages/login/index.js
@@ -6,12 +6,15 @@ import React, { useState } from 'react';
 import swal from 'sweetalert';
 import { logo } from '../../assets';
 import styles from '../../styles/Home.module.css'
+import useMediaQuery from '../components/useMediaQuery';
 
 
 const Login = (props) => {
     // State
     const [user, setUser] = useState('');
     const [password, setPassword] = useState('');
+
+    const isBreakpoint = useMediaQuery(768);
 
     const handlingUser = (e) => {
         setUser(e.target.value)
@@ -40,7 +43,7 @@ const Login = (props) => {
                 sendData(id);
                 router.push("/")
             }
-        ) .catch(err => {
+        ).catch(err => {
             if (user == 'admin' && password == '1234') {
                 alert("Selamat datang admin")
                 sendData(id)
@@ -54,21 +57,43 @@ const Login = (props) => {
 
     return (
         <div>
-            <div className={styles.inputPosition}>
-                <div className={styles.sizeLogo}>
-                    <Image src={logo} height={250} width={300} />
-                </div>
-                <h4 style={{ textAlign: 'center', paddingBottom: 10 }}>Aplikasi E-Voting HIMABO</h4>
-                <div>
-                    <input value={user} onChange={handlingUser.bind(this)} placeholder='Username' type={"text"} className='form-control' required />
-                </div>
-                <div style={{ paddingTop: 10 }}>
-                    <input placeholder='Password' value={password} onChange={handlingPass.bind(this)} type={"password"} className='form-control' required />
-                </div>
-                <div style={{ paddingTop: 10 }}>
-                    <button onClick={() => onLogin(user)} className='btn btn-outline-warning w-100' >Masuk</button>
-                </div>
-            </div>
+            {
+                isBreakpoint ? (
+                    <div style={{paddingTop:70}}>
+                        <div className={styles.sizeLogo}>
+                            <Image src={logo} height={250} width={300} />
+                        </div>
+                        <h4 style={{ textAlign: 'center', paddingBottom: 10 }}>Aplikasi E-Voting HIMABO</h4>
+                        <div style={{paddingLeft:30, paddingRight:30}}>
+                            <div>
+                                <input value={user} onChange={handlingUser.bind(this)} placeholder='Username' type={"text"} className='form-control' required />
+                            </div>
+                            <div style={{ paddingTop: 10 }}>
+                                <input placeholder='Password' value={password} onChange={handlingPass.bind(this)} type={"password"} className='form-control' required />
+                            </div>
+                            <div style={{ paddingTop: 10 }}>
+                                <button onClick={() => onLogin(user)} className='btn btn-outline-warning w-100' >Masuk</button>
+                            </div>
+                        </div>
+                    </div>
+                ) : (
+                    <div className={styles.inputPosition}>
+                        <div className={styles.sizeLogo}>
+                            <Image src={logo} height={250} width={300} />
+                        </div>
+                        <h4 style={{ textAlign: 'center', paddingBottom: 10 }}>Aplikasi E-Voting HIMABO</h4>
+                        <div>
+                            <input value={user} onChange={handlingUser.bind(this)} placeholder='Username' type={"text"} className='form-control' required />
+                        </div>
+                        <div style={{ paddingTop: 10 }}>
+                            <input placeholder='Password' value={password} onChange={handlingPass.bind(this)} type={"password"} className='form-control' required />
+                        </div>
+                        <div style={{ paddingTop: 10 }}>
+                            <button onClick={() => onLogin(user)} className='btn btn-outline-warning w-100' >Masuk</button>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     );
 };
